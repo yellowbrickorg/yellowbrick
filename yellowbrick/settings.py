b56336f3cc94dab9a780cd30185708857c8667e9
Adam Greloch <zplhatesbananas@gmail.com>
2023-04-06 11:10:19 +0200

    set up Postgres, bump Django version from 4.1.7 to 4.2

--- yellowbrick/settings.py
+++ yellowbrick/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'yellowbrick.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'yellowbrick_db',
+            'passfile': '.my_pgpass',
+        },
     }
 }
 
