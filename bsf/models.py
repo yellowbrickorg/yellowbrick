20ca29258986cf38b1075c3b630d007a0732e455
Maciek Kielak <maciek@kielak.com>
2023-04-11 17:37:08 +0200

    Brick model

--- bsf/models.py
+++ bsf/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Color(models.Model):
+    color_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    rgb = models.CharField(max_length=6)
+    is_transparent = models.BooleanField(default=False)
+class Brick(models.Model):
+    brick_id = models.IntegerField(primary_key=True)
+    part_num = models.CharField(max_length=30)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+
